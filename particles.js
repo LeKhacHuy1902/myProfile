9b4e6c1c160892bf8897502069cfd668d8570f24
LeKhacHuy1902 <lekhachuy1902@gmail.com>
2025-06-21 12:01:08 +0700

    2

--- particles.js
+++ particles.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
             },
             "move": {
                 "enable": true,
-                "speed": 1,
+                "speed": 1.5,
                 "direction": "none",
                 "random": false,
                 "straight": false,
